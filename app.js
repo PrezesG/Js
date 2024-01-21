bc5922dde099d0239c7376adf56a0179dbc7bb2e
PrezesG <129513239+PrezesG@users.noreply.github.com>
2024-01-21 08:09:00 +0100

    Fixed app.js

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ const app = express();
 const port = 3000;
 require('./src/db');
 const indexRoutes = require('./src/routes/index');
-const session = require('express-session');
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // set to true if your using https
-}));
+
+
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
